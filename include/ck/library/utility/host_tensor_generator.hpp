44f07c0ad826719551a3d34ee62a79f087ef7233
Kevin Abraham <kevin.abraham@streamhpc.com>
2025-09-26 21:02:09 +0200

    run clang-format-overwrite with pre-commit hooks

--- include/ck/library/utility/host_tensor_generator.hpp
+++ include/ck/library/utility/host_tensor_generator.hpp
@@ -499,7 +499,7 @@ struct GeneratorTensor_4
     std::normal_distribution<float> distribution;
 
     GeneratorTensor_4(float mean, float stddev, unsigned int seed = 1)
-        : generator(seed), distribution(mean, stddev){};
+        : generator(seed), distribution(mean, stddev) {};
 
     template <typename... Is>
     T operator()(Is...)
@@ -517,7 +517,7 @@ struct GeneratorTensor_4<ck::f4x2_pk_t>
     std::normal_distribution<float> distribution;
 
     GeneratorTensor_4(float mean, float stddev, unsigned int seed = 1)
-        : generator(seed), distribution(mean, stddev){};
+        : generator(seed), distribution(mean, stddev) {};
 
     template <typename... Is>
     ck::f4x2_pk_t operator()(Is...)
@@ -536,7 +536,7 @@ struct GeneratorTensor_4<ck::f6x32_pk_t>
     std::normal_distribution<float> distribution;
 
     GeneratorTensor_4(float mean, float stddev, unsigned int seed = 1)
-        : generator(seed), distribution(mean, stddev){};
+        : generator(seed), distribution(mean, stddev) {};
 
     template <typename... Is>
     ck::f6x32_pk_t operator()(Is...)
@@ -558,7 +558,7 @@ struct GeneratorTensor_4<ck::bf6x32_pk_t>
     std::normal_distribution<float> distribution;
 
     GeneratorTensor_4(float mean, float stddev, unsigned int seed = 1)
-        : generator(seed), distribution(mean, stddev){};
+        : generator(seed), distribution(mean, stddev) {};
 
     template <typename... Is>
     ck::bf6x32_pk_t operator()(Is...)
